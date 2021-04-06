38cf2de8201ff7197c19b870d0c3327a38ca8ceb
KikyTokamuro <kiky.tokamuro@yandex.ru>
2021-04-06 21:34:48 +0300

    Added configuration for Ocaml

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -112,3 +112,17 @@
 (add-hook 'c++-mode-hook 'my-c-mode-hook)
 (add-hook 'c-mode-hook 'my-c-mode-hook)
 
+
+;; Ocaml
+(let ((opam-share (ignore-errors (car (process-lines "opam" "config" "var" "share")))))
+  (when (and opam-share (file-directory-p opam-share))
+    (add-to-list 'load-path (expand-file-name "emacs/site-lisp" opam-share))
+    (autoload 'merlin-mode "merlin" nil t nil)
+    (add-hook 'tuareg-mode-hook 'merlin-mode t)
+    (add-hook 'caml-mode-hook 'merlin-mode t)
+    (setq merlin-command 'opam)))
+
+(with-eval-after-load 'company
+ (add-to-list 'company-backends 'merlin-company-backend))
+
+(add-hook 'merlin-mode-hook 'company-mode)
