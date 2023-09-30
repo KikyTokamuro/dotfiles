dfc22bfc5a88b87c69b171b01f034e7248e33d93
KikyTokamuro <kiky.tokamuro@yandex.ru>
2023-10-01 01:25:23 +0300

    Added emacs keyboard module

---
+++ .emacs.d/elisp/keyboard.el
@@ -0,0 +1,12 @@
+;;; keyboard.el --- Keyboard tools
+;; Author: Arhangelsky Daniil (Kiky Tokamuro) <kiky.tokamuro@yandex.ru>
+;;; Commentary:
+;;; Code:
+
+;; Reverse-im
+(use-package reverse-im
+  :ensure t
+  :config
+  (reverse-im-activate "russian-computer"))
+
+;; keyboard.el ends here
