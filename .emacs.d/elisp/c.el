c28b12c888d2b2dfc2c4724b6dc8c79a013bf23b
KikyTokamuro <kiky.tokamuro@yandex.ru>
2024-02-19 18:02:58 +0300

    Add c/c++ indentation

--- .emacs.d/elisp/c.el
+++ .emacs.d/elisp/c.el
@@ -5,6 +5,8 @@
 
 (defun my-c/c++-mode-hook ()
   "C/C++ mode hook."
+  (setq c-basic-offset 4)
+  (c-set-offset 'substatement-open 0)
   (eglot-ensure))
 
 (add-hook 'c-mode-hook 'my-c/c++-mode-hook)
